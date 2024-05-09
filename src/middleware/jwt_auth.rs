5db054e3208c73c83e3a7d200cd97785537b2197
noob <xiehenghong@163.com>
2024-05-10 01:29:43 +0800

    修补jwt_auth

--- src/middleware/jwt_auth.rs
+++ src/middleware/jwt_auth.rs
@@ -32,7 +32,6 @@ pub async fn jwt_auth_middleware(
         Err(err) => {
             res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
             res.body(err.to_string().into());
-            ctrl.skip_rest();
             return;
         }
     };
@@ -42,7 +41,6 @@ pub async fn jwt_auth_middleware(
         Err(_) => {
             res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
             res.body("JWT token not found in Depot".into());
-            ctrl.skip_rest();
             return;
         }
     };
@@ -50,9 +48,7 @@ pub async fn jwt_auth_middleware(
     let jwt_claims = match jwt::parse_token(&token) {
         Ok(claims) => claims,
         Err(err) => {
-            res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
-            res.body(err.to_string().into());
-            ctrl.skip_rest();
+             handle_internal_server_error(res, StatusCode::INTERNAL_SERVER_ERROR, err.to_string());
             return;
         }
     };
@@ -60,9 +56,7 @@ pub async fn jwt_auth_middleware(
     let uri: Uri = match req.uri().to_string().parse() {
         Ok(uri) => uri,
         Err(err) => {
-            res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
-            res.body(err.to_string().into());
-            ctrl.skip_rest();
+             handle_internal_server_error(res, StatusCode::INTERNAL_SERVER_ERROR, err.to_string());
             return;
         }
     };
@@ -74,42 +68,38 @@ pub async fn jwt_auth_middleware(
             let check_result = match check_user_admin(&jwt_claims.user_id).await {
                 Ok(admin_res) => admin_res,
                 Err(err) => {
-                    res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
-                    res.body(err.to_string().into());
-                    ctrl.skip_rest();
+                    handle_unauthorized_access(res, ctrl);
                     return;
                 }
             };
             if !is_admin_route(&uri.path()) {
                 // 如果访问的路径不在 admin 路由中，则拒绝访问
-                res.status_code(StatusCode::UNAUTHORIZED);
-                res.body("越权访问或路径不存在".to_string().into());
-                ctrl.skip_rest();
+                handle_unauthorized_access(res, ctrl);
+                return;
             }
-            // if uri.path() == "/listCustom" {
-            //     if 
-            // }
+            if is_super_admin_route(&uri.path()) {
+                // 如果访问的路径是超级管理员路由，则检查用户是否是超级管理员
+                if check_result.role != 0 {
+                    handle_unauthorized_access(res, ctrl);
+                    return;
+                }
+            }
         }
         None => {
             // 查看custom表中是否有这个用户
             let check_result = match check_user_custom(&jwt_claims.user_id).await {
-                    Ok(custom_res) => 
-                        // 如果用户存在，则返回结构体
-                         custom_res,
-                    
-                    Err(err) => {
-                        // 如果用户不存在，则进行错误处理
-                        res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
-                        res.body(err.to_string().into());
-                        ctrl.skip_rest();
-                        return;
-                    }
+                // 如果用户存在，则返回结构体
+                Ok(custom_res) => custom_res,
+                Err(err) => {
+                    // 如果用户不存在，则进行错误处理
+                    handle_unauthorized_access(res, ctrl);
+                    return;
+                }
             };
             if !is_custom_route(&uri.path()) {
                 // 如果访问的路径不在 custom 路由中，则拒绝访问
-                res.status_code(StatusCode::UNAUTHORIZED);
-                res.body("越权访问或路径不存在".to_string().into());
-                ctrl.skip_rest();
+                handle_unauthorized_access(res, ctrl);
+                return;
             }
         }
     }
@@ -121,4 +111,19 @@ fn is_custom_route(uri_path: &str) -> bool {
 
 fn is_admin_route(uri_path: &str) -> bool {
     uri_path.starts_with("/admin/api/")
+}
+
+fn is_super_admin_route(uri_path: &str) -> bool {
+    uri_path.starts_with("/admin/api/manager/")
+}
+
+fn handle_internal_server_error(res: &mut Response, status_code: StatusCode, message: String) {
+    res.status_code(status_code);
+    res.body(message.into());
+}
+
+fn handle_unauthorized_access(res: &mut Response, ctrl: &mut FlowCtrl) {
+    res.status_code(StatusCode::UNAUTHORIZED);
+    res.body("越权访问或路径不存在".to_string().into());
+    ctrl.skip_rest();
 }
