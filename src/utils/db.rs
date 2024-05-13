c7ff12925082abe97bc22953bce0c8f7a2b3b462
pc <debrisfox@163.com>
2024-05-13 23:26:09 +0800

    change config file

--- src/utils/db.rs
+++ src/utils/db.rs
@@ -3,7 +3,6 @@ use sea_orm::{entity::prelude::DatabaseConnection, ConnectOptions, Database};
 use std::time::Duration;
 use tokio::sync::OnceCell;
 pub static DB: OnceCell<DatabaseConnection> = OnceCell::const_new();
-
 pub async fn init_db_conn() {
     DB.get_or_init(|| async {
         let mut opt = ConnectOptions::new(CFG.database.database_url.to_owned());
@@ -11,8 +10,7 @@ pub async fn init_db_conn() {
             .min_connections(5)
             .connect_timeout(Duration::from_secs(8))
             .idle_timeout(Duration::from_secs(8))
-            .sqlx_logging(false);
-
+            .sqlx_logging(true);
         Database::connect(opt).await.expect("数据库打开失败")
     })
     .await;
